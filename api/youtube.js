5027db70ba3106f2721a8fcd47113be4dba133cf
Dan <danpetersen@hotmail.co.uk>
2022-07-25 11:34:12 +0100

    initial version using vite & react

---
+++ api/youtube.js
@@ -0,0 +1,14 @@
+const youtube = {
+    baseURL:'www.googleapis.com/youtube/v3',
+    meta: {
+                headers: {
+                "Authorization": "Client-ID 6FeYbuLG-6MhqNTKxko8Elk2V_hyvEkS-jOz9M-V5ls",
+                "Method": "GET",
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+        }
+    
+}
+
+export default UnSplash ;
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hello  from './components/Hello'
+
+class App extends React.Component {
+  state = {  
+  };
+
+  render() {
+    return (
+      <div>
+           <Hello />
+    </div>
+    );
+  }
+
+}
+
+export default App
