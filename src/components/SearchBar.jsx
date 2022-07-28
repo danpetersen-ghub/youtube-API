3c6cf94c9fecbcccf14f9e84c38658f93b99d9c8
Dan <danpetersen@hotmail.co.uk>
2022-07-28 07:55:08 +0100

    updates to Search component & created sequece uml

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,24 +4,33 @@ class SearchBar extends React.Component {
     state = {  
       InputValue: '',
     };
+
+    submitFunction(e)  {
+      e.preventDefault();
+      console.log(this.state);
+    //   document.addEventListener('keydown', (e) => {
+    //     if(e.key === 'Enter') {
+    //       let searchValue = document.getElementById('search-input').value;
+    //       this.setState({ InputValue: searchValue });
+    //       console.log(this.state);
+    //     }      
+    //  });
+    }
     
     render() {
-        document.addEventListener('keydown', (e) => {
-          if(e.key === 'Enter') {
-            let searchValue = document.getElementById('search-input').value;
-            this.setState({ InputValue: searchValue });
-            console.log(this.state);
-          }      
-       });
+
       return (
         <div>
-            <form>
-              <h5>Title</h5>
-              <div className="md-form mt-10 mb-100 text-uppercase">
-                <input type="text" id="search-input" className="form-control" />
+            <form onSubmit={(e) => this.submitFunction(e)}>
+              <h1>Video Search</h1>
+              <div  className="md-form mt-10 mb-100 text-uppercase">
+                <input  type="text" id="search-input" className="form-control"  value={this.state.InputValue} onChange={ (e) => this.setState({InputValue: e.target.value}) } />
               </div>
-              <i>Showing Results for: {this.state.InputValue}</i>
-            </form>
+              <h5>
+                <i>Showing Results for: {this.state.InputValue}</i>
+              </h5>
+             </form>
+             <hr />
       </div>
       );
     }
