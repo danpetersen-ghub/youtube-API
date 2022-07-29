89eccea3cf61da1201d7289e6ded97c830fdcf3b
Dan <danpetersen@hotmail.co.uk>
2022-07-29 09:35:56 +0100

    api work

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,24 @@ import VideoList from './components/VideoList'
 import VideoDetail from './components/VideoDetail'
 import VideoItem from './components/VideoItem'
 
+import youtubeAPI from './api/youtube'
+
 class App extends React.Component {
-  state = {  
+  state = { 
+    videos: [], 
   };
+
+  getVideos = async ( term ) => {
+  const response = await fetch(`${youtubeAPI.baseurl}?${term}`, youtubeAPI.meta).then(res => res.json());
+  this.setState({ videos: response });
+  }
 
   render() {
     return (
       <div>
            {/* <Hello /> */}
-           <SearchBar />
-            <VideoList />
+           <SearchBar getVideos={this.getVideos} />
+            <VideoList videos={this.state.videos} />
             <VideoDetail />
             <VideoItem />
     </div>
